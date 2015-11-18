42403b0a4a7f1eba086728bf419745a5deec9dd0
wuman0173 <bwu0173@yahoo.com>
2015-11-18 17:33:53 -0500

    Update DirectoryDisplay.java

--- DirectoryDisplay.java
+++ DirectoryDisplay.java
@@ -1,1 +1,21 @@
+/* Benjamin Wu
+ * CISC 3150 HW7
+ * Display Directory Tree
+ *
+ * Comments: Got help from websites to begin working on how to display the directories in the computer.
+ *
+ * http://www.java-examples.com/list-contents-directory
+ * http://www.avajava.com/tutorials/files/how-do-i-recursively-display-all-files-and-directories-in-a-directory/RecursiveFileDisplay.java
+ *
+ */
 
+import java.io.*;
+
+public class DirectoryDisplay {
+
+	public static void main(String[] args){
+		File AllFiles = new File("../../../");
+	
+
+	}
+}
